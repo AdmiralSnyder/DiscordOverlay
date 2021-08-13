a20f064d97d8f56ea65a5e86653f9f4e6aa458ce
Alexander Gayko <a.gayko@googlemail.com>
2021-08-13 18:38:38 +0200

    vs2022 support - let's see if the hardcoded x64 can be circumvented. see https://github.com/MicrosoftEdge/WebView2Feedback/issues/182

--- VSDiscordStatus/DiscordStatusUserControl.xaml.cs
+++ VSDiscordStatus/DiscordStatusUserControl.xaml.cs
@@ -21,17 +21,24 @@ namespace VSDiscordStatus
         private bool Initialized = false;
         private async void MyToolWindow_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            if (!Initialized)
+            try
             {
-                if (IsVisible)
+                if (!Initialized)
                 {
-                    Initialized = true;
-                    await LeftWebView.EnsureCoreWebView2Async().ConfigureAwait(true);
-                    LeftWebView.Source = new Uri("https://streamkit.discord.com/overlay/voice/143867839282020352/827982833888526366?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=14&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=0.95&bg_shadow_color=%23000000&bg_shadow_size=0&invite_code=csharp&limit_speaking=false&small_avatars=false&hide_names=false&fade_chat=0");
+                    if (IsVisible)
+                    {
+                        Initialized = true;
+                        await LeftWebView.EnsureCoreWebView2Async().ConfigureAwait(true);
+                        LeftWebView.Source = new Uri("https://streamkit.discord.com/overlay/voice/143867839282020352/827982833888526366?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=14&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=0.95&bg_shadow_color=%23000000&bg_shadow_size=0&invite_code=csharp&limit_speaking=false&small_avatars=false&hide_names=false&fade_chat=0");
 
-                    await RightWebView.EnsureCoreWebView2Async().ConfigureAwait(true);
-                    RightWebView.Source = new Uri("https://streamkit.discord.com/overlay/voice/143867839282020352/827982932311408650?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=8&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=1&bg_shadow_color=%23000000&bg_shadow_size=0&invite_code=csharp&limit_speaking=false&small_avatars=true&hide_names=false&fade_chat=0");
+                        await RightWebView.EnsureCoreWebView2Async().ConfigureAwait(true);
+                        RightWebView.Source = new Uri("https://streamkit.discord.com/overlay/voice/143867839282020352/827982932311408650?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=8&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=1&bg_shadow_color=%23000000&bg_shadow_size=0&invite_code=csharp&limit_speaking=false&small_avatars=true&hide_names=false&fade_chat=0");
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString(), "Error occurred in " + nameof(MyToolWindow_IsVisibleChanged))
             }
         }
     }
